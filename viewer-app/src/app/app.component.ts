0be9b84e3878d7c1417e6a201898a1c0ee35048c
David Gonzalez Sola <David.Gonzalez.Sola@unit4.com>
2020-06-14 20:11:19 +0200

    Add Azure Function for handling CloudEvents and pushing them to the SignalR service. Add Angular application for connecting to the SignalR service

---
+++ viewer-app/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import * as SignalR from '@aspnet/signalr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'viewer-app';
+
+  private hubConnection: SignalR.HubConnection;
+
+  constructor() {
+    // Create connection
+    this.hubConnection = new SignalR.HubConnectionBuilder()
+        .withUrl("http://localhost:7071/api")
+        .build();
+    
+    // Start connection. This will call negotiate endpoint
+    this.hubConnection
+        .start();
+    
+    // Handle incoming events for the specific target
+    this.hubConnection.on("newEvent", (event) => {
+        console.log(`Event received: ${event}`)
+    });
+  }
+}
